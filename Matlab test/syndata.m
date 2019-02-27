9f732737f3de4f2a51d76fc1904800133ba54be7
AMCA15 <38790021+AMCA15@users.noreply.github.com>
2019-02-27 16:30:09 -0400

    Unpacking frames

--- Matlab test/syndata.m
+++ Matlab test/syndata.m
@@ -1,16 +1,33 @@
-start = find(data > 0, 1);
-syncData = data(start:end-(5-start));
+
+data = fread(s,512,'int8');
+databyte = int8(data);
+
+start = find(databyte > 0, 1);
+syncData = databyte(start:end-start+1);
+
+syncData = typecast(syncData, 'uint8');
+
+blocksize = length(syncData);
+
+byte0 = syncData(1:4:end);
+byte1 = syncData(2:4:end);
+byte2 = syncData(3:4:end);
+byte3 = syncData(4:4:end);
 
 
-byte3 = syncData(1:4:end);
-byte2 = syncData(2:4:end);
-byte1 = syncData(3:4:end);
-byte0 = syncData(4:4:end);
+cha_d = bitget(byte1, 7);
+chb_d = bitget(byte3, 7);
 
+Mask_Ana(1:blocksize/4,1) = uint8(63);
 
-cha_d = bitget(byte1, 7, 'int8');
-chb_d = bitget(byte3, 7, 'int8');
-%cha_a_l = bitsliceget(byte2, 6);
-%cha_a_h = bitsliceget(byte1, 6);
-%cha_b_l = bitsliceget(byte2, 6);
-%cha_b_h = bitsliceget(byte3, 6);
+cha_a_l = bitand(byte0, Mask_Ana);
+cha_a_h = bitand(byte1, Mask_Ana);
+chb_a_l = bitand(byte2, Mask_Ana);
+chb_a_h = bitand(byte3, Mask_Ana);
+
+t = 1:blocksize/4;
+
+cha_a = uint16(cha_a_h)*64 + uint16(cha_a_l);
+chb_a = uint16(chb_a_h)*64 + uint16(chb_a_l);
+
+plot(t, cha_a, t, chb_a);
