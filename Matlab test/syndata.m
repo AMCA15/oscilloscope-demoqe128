c33751fb86a909166bcb1c73bf3286ecbbb3f0b7
alvaroanavarro1 <38303261+alvaroanavarro1@users.noreply.github.com>
2019-02-27 12:14:06 -0400

    script update
    Se modifico la extración de bits para los canales digitales, ademas de la composición para los canales analogicos a y b

--- Matlab test/syndata.m
+++ Matlab test/syndata.m
@@ -1,16 +1,26 @@
 start = find(data > 0, 1);
 syncData = data(start:end-(5-start));
 
-
+%Extrae los bytes de la data
 byte3 = syncData(1:4:end);
 byte2 = syncData(2:4:end);
 byte1 = syncData(3:4:end);
 byte0 = syncData(4:4:end);
 
-
+%Extrae los bits de los canales digitales
 cha_d = bitget(byte1, 7, 'int8');
 chb_d = bitget(byte3, 7, 'int8');
-%cha_a_l = bitsliceget(byte2, 6);
-%cha_a_h = bitsliceget(byte1, 6);
-%cha_b_l = bitsliceget(byte2, 6);
-%cha_b_h = bitsliceget(byte3, 6);
+
+%Configura los bits para el canal analogico a
+cha_a_l = dec2bin(typecast(byte0,'uint8'));
+cha_a_low = cha_a_l(:,1:6);
+cha_a_h = dec2bin(typecast(byte1,'uint8'));
+cha_a_high = cha_a_h(:,1:6);
+cah_a = [cha_a_high cha_a_low];
+
+%Configura los bits para el canal analogico b
+cha_b_l = dec2bin(typecast(byte2,'uint8'));
+cha_b_low = cha_b_l(:,1:6);
+cha_b_h = dec2bin(typecast(byte3,'uint8'));
+cha_b_high = cha_b_h(:,1:6);
+cha_b = [cha_b_high cha_b_low];
